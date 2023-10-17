a9c3deaa7bb8f4916bbb1baebb8e8dd568a86e50
Rafael Correa <rafaeldev3590@gmail.com>
2023-10-17 20:43:07 -0300

    Criacao do projeto

---
+++ apiWebQuest/src/main/java/br/com/apiWebQuest/ApiWebQuestApplication.java
@@ -0,0 +1,13 @@
+package br.com.apiWebQuest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWebQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWebQuestApplication.class, args);
+	}
+
+}
---
+++ apiWebQuest/src/test/java/br/com/apiWebQuest/ApiWebQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apiWebQuest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWebQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
